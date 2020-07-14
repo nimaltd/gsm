537a62ad5f7d9fb3fa5a35e2248516f3da9f63ab
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-07-14 10:35:02 +0430

    Update gsmCallback.c

--- gsmCallback.c
+++ gsmCallback.c
@@ -2,7 +2,6 @@
 #include "gsm.h"
 #include "functions.h"
 
-char  dtmf[25];
 //#################################################################################
 __weak void gsm_callback_simcardReady(void)
 {
@@ -22,35 +21,11 @@ __weak void gsm_callback_simcardPukRequest(void)
 __weak void gsm_callback_newMsg(char *number, Gsm_Time_t time, char *msg)
 {
   //gsm_msg_send(number, "I got a message");
-  if (strcmp((char*)ADMIN_NUMBER, number) == 0)
-  {
-    smsInstruction(number, msg);
-    return;
-  }
-  for (uint8_t i = 0; i < PHONEBOOK_MAX; i++)
-  {
-    if (strcmp((char*)phonebook[i], number) == 0)
-      smsInstruction(number, msg);
-  }
 }
 //#################################################################################
 __weak void gsm_callback_newCall(char *number)
 {
   //gsm_call_answer();
-  if (strcmp((char*)ADMIN_NUMBER, number) == 0)
-  {
-    gsm_call_answer();
-    return;
-  }
-  for (uint8_t i = 0; i < PHONEBOOK_MAX; i++)
-  {
-    if (strcmp((char*)phonebook[i], number) == 0)
-    {
-      gsm_call_answer();
-      return;
-    }
-  }
-  gsm_call_end();
 }
 //#################################################################################
 __weak void gsm_callback_endCall(void)
@@ -60,6 +35,6 @@ __weak void gsm_callback_endCall(void)
 //#################################################################################
 __weak void gsm_callback_dtmf(char key, uint8_t cnt)  //  do not use any AT-COMMAND and gsm functions here
 {
-  dtmf[cnt] = key;
+
 }
 //#################################################################################
